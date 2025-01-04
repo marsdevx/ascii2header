7447a4cae68a51e6ff4027d8b140e0670bc865cc
GNUS <60074926+theMark001@users.noreply.github.com>
2025-01-04 06:51:10 +0100

    feat: modify func to return all data as dict

--- get_info.py
+++ get_info.py
@@ -3,15 +3,23 @@ import subprocess
 from datetime import datetime
 
 def get_info(path):
+  info = {
+    "file_name": "",
+    "git_repo": "",
+    "git_user": "",
+    "creation_time": "",
+    "update_time": "",
+    "rel_path": ""
+  }
   file_name = os.path.basename(path)
-  print(file_name)
+  info['file_name'] = file_name
 
   creation_timestamp = subprocess.check_output(["stat", "-f", "%B", path]).decode().strip()
   creation_time = datetime.fromtimestamp(int(creation_timestamp)).strftime("%H:%M   %d/%m/%Y")
-  print(creation_time)
+  info['creation_time'] = creation_time
 
   update_time = datetime.fromtimestamp(os.stat(path).st_mtime).strftime("%H:%M   %d/%m/%Y")
-  print(update_time)
+  info['update_time'] = update_time
 
   git_url = subprocess.check_output(["git", "remote", "get-url", "origin"], cwd=os.path.dirname(path)).decode().strip()
   if git_url.startswith("https://"):
@@ -23,11 +31,15 @@ def get_info(path):
   else:
     git_user = "unknown"
     git_repo = "unknown"
-  print(git_repo)
-  print(git_user)
+
+  info['git_user'] = git_user
+  info['git_repo'] = git_repo
+
 
   rel_path = os.path.relpath(path, git_repo)[1:]
-  print(rel_path)
+  info['rel_path'] = rel_path
+  
+  return info
 
 path = os.path.expanduser("~/Desktop/Projects/ascii2header/header_example.txt")
-get_info(path)
+print(get_info(path))
