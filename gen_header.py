35af6a12b341bc298f0d711a3d43cd8e61e3ffd6
GNUS <60074926+theMark001@users.noreply.github.com>
2025-01-04 21:06:47 +0100

    feat: add func to gen header based on file type and ascii art

--- gen_header.py
+++ gen_header.py
@@ -2,17 +2,37 @@ import os
 import gen_header_txt
 import get_info
 
-# So i need to find all of the borders than i need and write func to generate header string var, based on the file type
+def gen_header(header_core, file_ext):
+  if file_ext in [".c", ".css", ".js", ".ino", ".h"]:
+    start_marker = "/*   "
+    end_marker = "  */"
+  else:
+    start_marker = "#    "
+    end_marker = "   #"
+
+  header_lines = header_core.splitlines()
+
+  header = []
+  for line in header_lines:
+    decorated_line = f"{start_marker}{line.ljust(116)}{end_marker}"
+    header.append(decorated_line)
+  header = "\n".join(header)
+
+  return header
+
+
+
 
 
 file_path = os.path.expanduser("~/Desktop/Projects/ascii2header/header_example.txt")
 ascii_art_path = os.path.expanduser("~/Desktop/Projects/ascii2header/anime_girl.txt")
 info = get_info.header_info(file_path)
 
-_, file_extension = os.path.splitext(info['file_name'])
-print(file_extension)
-
+_, file_ext = os.path.splitext(info['file_name'])
 header_core = gen_header_txt.generate_header_core(info, ascii_art_path)
+
+header = gen_header(header_core, file_ext)
+
 output_file = "generated_header.txt"
 with open(output_file, "w", encoding="utf-8") as file:
-  file.write(header_core)
+  file.write(header)
