728c99767cb97ed753fc82af984577e61eee7aa7
GNUS <60074926+theMark001@users.noreply.github.com>
2025-01-04 20:42:19 +0100

    feat: add get file ext

--- gen_header.py
+++ gen_header.py
@@ -2,11 +2,17 @@ import os
 import gen_header_txt
 import get_info
 
+# So i need to find all of the borders than i need and write func to generate header string var, based on the file type
+
+
 file_path = os.path.expanduser("~/Desktop/Projects/ascii2header/header_example.txt")
 ascii_art_path = os.path.expanduser("~/Desktop/Projects/ascii2header/anime_girl.txt")
 info = get_info.header_info(file_path)
 
-header = gen_header_txt.generate_header_core(info, ascii_art_path)
+_, file_extension = os.path.splitext(info['file_name'])
+print(file_extension)
+
+header_core = gen_header_txt.generate_header_core(info, ascii_art_path)
 output_file = "generated_header.txt"
 with open(output_file, "w", encoding="utf-8") as file:
-  file.write(header)
+  file.write(header_core)
