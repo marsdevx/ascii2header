5c1e165cf7e6a1d0f9143cc787018a83fdb10bb9
GNUS <60074926+theMark001@users.noreply.github.com>
2025-01-04 07:52:33 +0100

    feat: modify gen header to print info at the middle of the header

--- gen_header.py
+++ gen_header.py
@@ -1,4 +1,5 @@
 import os
+import math
 import get_info
 
 def generate_header(info, ascii_art_path):
@@ -9,20 +10,24 @@ def generate_header(info, ascii_art_path):
 
   with open(ascii_art_path, 'r', encoding='utf-8') as file:
     ascii_art_path = file.readlines()
-  
-  for key, value in info.items():
-    info_line = value.ljust(width // 2) 
-    art_line = ascii_art_path[ascii_index].strip() if ascii_index < len(ascii_art_path) else ''
-    combined_line = f"{info_line}{art_line.rjust(width - len(info_line))}"  
-    header_lines.append(combined_line)
-    ascii_index += 1
 
-  while ascii_index < len(ascii_art_path):
-    art_line = ascii_art_path[ascii_index].strip()
-    empty_info = ' ' * (width // 2)
-    combined_line = f"{empty_info}{art_line.rjust(width - len(empty_info))}"
-    header_lines.append(combined_line)
-    ascii_index += 1
+  ascii_lines = len(ascii_art_path)
+  start_point = math.floor((ascii_lines - 9) / 2)
+  
+  for i in range(ascii_lines):
+    if i >= start_point and i < start_point + len(info):
+      info_key = list(info.keys())[ascii_index]
+      info_value = info[info_key]
+      info_line = info_value.ljust(width // 2)
+      art_line = ascii_art_path[i].strip() if i < len(ascii_art_path) else ''
+      combined_line = f"{info_line}{art_line.rjust(width - len(info_line))}"
+      header_lines.append(combined_line)
+      ascii_index += 1
+    else:
+      art_line = ascii_art_path[i].strip()
+      empty_info = ' ' * (width // 2)
+      combined_line = f"{empty_info}{art_line.rjust(width - len(empty_info))}"
+      header_lines.append(combined_line)
 
   return '\n'.join(header_lines)
 
