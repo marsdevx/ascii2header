fc82c4d16fe988a21aeed204aa862cc6f8cd33c3
GNUS <60074926+theMark001@users.noreply.github.com>
2025-01-04 07:10:49 +0100

    feat: update dic values to math the info template

--- get_info.py
+++ get_info.py
@@ -12,14 +12,14 @@ def header_info(path):
     "rel_path": ""
   }
   file_name = os.path.basename(path)
-  info['file_name'] = file_name
+  info['file_name'] = f"File:         {file_name}"
 
   creation_timestamp = subprocess.check_output(["stat", "-f", "%B", path]).decode().strip()
   creation_time = datetime.fromtimestamp(int(creation_timestamp)).strftime("%H:%M   %d/%m/%Y")
-  info['creation_time'] = creation_time
+  info['creation_time'] = f"Created:      {creation_time}"
 
   update_time = datetime.fromtimestamp(os.stat(path).st_mtime).strftime("%H:%M   %d/%m/%Y")
-  info['update_time'] = update_time
+  info['update_time'] = f"Updated:      {update_time}"
 
   git_url = subprocess.check_output(["git", "remote", "get-url", "origin"], cwd=os.path.dirname(path)).decode().strip()
   if git_url.startswith("https://"):
@@ -32,11 +32,11 @@ def header_info(path):
     git_user = "unknown"
     git_repo = "unknown"
 
-  info['git_user'] = git_user
-  info['git_repo'] = git_repo
+  info['git_user'] = f"Github:       {git_user}"
+  info['git_repo'] = f"Project:      {git_repo}"
 
 
   rel_path = os.path.relpath(path, git_repo)[1:]
-  info['rel_path'] = rel_path
+  info['rel_path'] = f"Path:         {rel_path}"
   
   return info
