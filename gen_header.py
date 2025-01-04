d9d24482a6d0f84f7884efbcd18817d53dfd6da5
GNUS <60074926+theMark001@users.noreply.github.com>
2025-01-04 21:12:51 +0100

    refactor: merge gen_info.py gen_header_txt.py gen_header.py to gen_header.py

--- gen_header.py
+++ gen_header.py
@@ -1,6 +1,74 @@
 import os
-import gen_header_txt
-import get_info
+import math
+import subprocess
+from datetime import datetime
+
+def header_info(path):
+  info = {
+    "file_name": "",
+    "empty1": "",
+    "git_repo": "",
+    "git_user": "",
+    "empty2": "",
+    "creation_time": "",
+    "update_time": "",
+    "empty3": "",
+    "rel_path": ""
+  }
+  
+  file_name = os.path.basename(path)
+  info['file_name'] = f"File:         {file_name}"
+
+  creation_timestamp = subprocess.check_output(["stat", "-f", "%B", path]).decode().strip()
+  creation_time = datetime.fromtimestamp(int(creation_timestamp)).strftime("%H:%M   %d/%m/%Y")
+  info['creation_time'] = f"Created:      {creation_time}"
+
+  update_time = datetime.fromtimestamp(os.stat(path).st_mtime).strftime("%H:%M   %d/%m/%Y")
+  info['update_time'] = f"Updated:      {update_time}"
+
+  git_url = subprocess.check_output(["git", "remote", "get-url", "origin"], cwd=os.path.dirname(path)).decode().strip()
+  if git_url.startswith("https://"):
+    git_user = git_url.split("/")[-2]
+    git_repo = git_url.split("/")[-1].replace(".git", "")
+  else:
+    git_user = "unknown"
+    git_repo = "unknown"
+  info['git_user'] = f"Github:       {git_user}"
+  info['git_repo'] = f"Project:      {git_repo}"
+
+  rel_path = os.path.relpath(path, git_repo)[1:]
+  info['rel_path'] = f"Path:         {rel_path}"
+  
+  return info
+
+def generate_header_core(info, ascii_art_path):
+
+  width = 115
+  header_lines = []
+  ascii_index = 0
+
+  with open(ascii_art_path, 'r', encoding='utf-8') as file:
+    ascii_art_path = file.readlines()
+
+  ascii_lines = len(ascii_art_path)
+  start_point = math.floor((ascii_lines - 9) / 2)
+  
+  for i in range(ascii_lines):
+    if i >= start_point and i < start_point + len(info):
+      info_key = list(info.keys())[ascii_index]
+      info_value = info[info_key]
+      info_line = info_value.ljust(width // 2)
+      art_line = ascii_art_path[i].strip() if i < len(ascii_art_path) else ''
+      combined_line = f"{info_line}{art_line.rjust(width - len(info_line))}"
+      header_lines.append(combined_line)
+      ascii_index += 1
+    else:
+      art_line = ascii_art_path[i].strip()
+      empty_info = ' ' * (width // 2)
+      combined_line = f"{empty_info}{art_line.rjust(width - len(empty_info))}"
+      header_lines.append(combined_line)
+
+  return '\n'.join(header_lines)
 
 def gen_header(header_core, file_ext):
   if file_ext in [".c", ".css", ".js", ".ino", ".h"]:
@@ -21,15 +89,12 @@ def gen_header(header_core, file_ext):
   return header
 
 
-
-
-
-file_path = os.path.expanduser("~/Desktop/Projects/ascii2header/header_example.txt")
+file_path = os.path.expanduser("~/Desktop/Projects/ascii2header/gen_header.py")
 ascii_art_path = os.path.expanduser("~/Desktop/Projects/ascii2header/anime_girl.txt")
-info = get_info.header_info(file_path)
+info = header_info(file_path)
 
 _, file_ext = os.path.splitext(info['file_name'])
-header_core = gen_header_txt.generate_header_core(info, ascii_art_path)
+header_core = generate_header_core(info, ascii_art_path)
 
 header = gen_header(header_core, file_ext)
 
